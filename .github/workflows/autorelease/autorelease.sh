1e565e8678f662c11d95eac904e71c5156b08f65
Abdulla Abdurakhmanov <me@abdolence.dev>
2022-09-17 16:51:30 +0200

    Auto release tests

--- .github/workflows/autorelease/autorelease.sh
+++ .github/workflows/autorelease/autorelease.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+if [[ -z "${GH_PUSH_TOKEN}" ]]; then
+  echo "Env GH_PUSH_TOKEN must be specified"
+  exit 1
+fi
+
 git submodule foreach git pull origin master
 cargo protosgen
 git add gcloud-sdk/genproto/*
-git --no-pager diff gcloud-sdk/genproto
 
-#cd gcloud-sdk
-#cargo release --package gcloud-sdk patch --no-dev-version --dry-run
-#cd ..
+git update-index --refresh
+git diff-index --quiet HEAD gcloud-sdk/genproto
+PROTOS_CHANGED=$?
+
+CURRENT_DATE=$(date '+%Y-%m-%d')
+
+if [[ PROTOS_CHANGED -eq 1 ]]; then
+  echo "Found changes in Google APIs to release"
+  cd gcloud-sdk || exit
+  git commit -m "Google APIs updated at ${CURRENT_DATE}"
+  cargo release --package gcloud-sdk patch --no-dev-version --token $GH_PUSH_TOKEN
+  cd ..
+else
+  echo "No changes are found in protos to release"
+fi
