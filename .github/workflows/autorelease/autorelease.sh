4c49eb03e46b310a710f93c55718b34dd6562a22
Abdulla Abdurakhmanov <me@abdolence.dev>
2022-09-17 16:32:46 +0200

    Test autorelease

--- .github/workflows/autorelease/autorelease.sh
+++ .github/workflows/autorelease/autorelease.sh
@@ -3,7 +3,7 @@
 git submodule foreach git pull origin master
 cargo protosgen
 git add gcloud-sdk/genproto/*
-git diff gcloud-sdk/genproto
+git --no-pager diff gcloud-sdk/genproto
 
 #cd gcloud-sdk
 #cargo release --package gcloud-sdk patch --no-dev-version --dry-run
