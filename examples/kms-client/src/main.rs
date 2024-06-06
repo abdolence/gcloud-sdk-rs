86cd5168900c67f12ddfd34cafe86a3c5e1a71bd
Abdulla Abdurakhmanov <me@abdolence.dev>
2024-06-06 17:50:53 +0200

    Formatting

--- examples/kms-client/src/main.rs
+++ examples/kms-client/src/main.rs
@@ -1,6 +1,6 @@
 use gcloud_sdk::google::cloud::kms::v1::key_management_service_client::KeyManagementServiceClient;
-use gcloud_sdk::*;
 use gcloud_sdk::google::cloud::kms::v1::ListCryptoKeysRequest;
+use gcloud_sdk::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,19 +10,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .finish();
     tracing::subscriber::set_global_default(subscriber)?;
 
-    let client: GoogleApi<KeyManagementServiceClient<GoogleAuthMiddleware>> = GoogleApi::from_function(
-        KeyManagementServiceClient::new,
-        "https://cloudkms.googleapis.com",
-        // cloud resource prefix: used only for some of the APIs (such as Firestore)
-        None,
-    )
-    .await?;
+    let client: GoogleApi<KeyManagementServiceClient<GoogleAuthMiddleware>> =
+        GoogleApi::from_function(
+            KeyManagementServiceClient::new,
+            "https://cloudkms.googleapis.com",
+            // cloud resource prefix: used only for some of the APIs (such as Firestore)
+            None,
+        )
+        .await?;
 
     let response = client
         .get()
         .list_crypto_keys(tonic::Request::new(ListCryptoKeysRequest {
             parent: "projects/your-project/locations/global/keyRings/your-key-ring".to_string(),
-            .. Default::default()
+            ..Default::default()
         }))
         .await?;
     println!("Response: {:?}", response);
