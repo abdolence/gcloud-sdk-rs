91d5f63d1dccde8ef6076a8e1f1e9e0bbc04edb5
Abdulla Abdurakhmanov <me@abdolence.dev>
2024-06-06 17:49:32 +0200

    kms client example

---
+++ examples/kms-client/src/main.rs
@@ -0,0 +1,31 @@
+use gcloud_sdk::google::cloud::kms::v1::key_management_service_client::KeyManagementServiceClient;
+use gcloud_sdk::*;
+use gcloud_sdk::google::cloud::kms::v1::ListCryptoKeysRequest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Debug logging
+    let subscriber = tracing_subscriber::fmt()
+        .with_env_filter("gcloud_sdk=debug")
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    let client: GoogleApi<KeyManagementServiceClient<GoogleAuthMiddleware>> = GoogleApi::from_function(
+        KeyManagementServiceClient::new,
+        "https://cloudkms.googleapis.com",
+        // cloud resource prefix: used only for some of the APIs (such as Firestore)
+        None,
+    )
+    .await?;
+
+    let response = client
+        .get()
+        .list_crypto_keys(tonic::Request::new(ListCryptoKeysRequest {
+            parent: "projects/your-project/locations/global/keyRings/your-key-ring".to_string(),
+            .. Default::default()
+        }))
+        .await?;
+    println!("Response: {:?}", response);
+
+    Ok(())
+}
