39fd04b90d2f189bcf28bf835e80d0274e060d91
Abdulla Abdurakhmanov <me@abdolence.dev>
2024-11-25 13:23:34 +0100

    iam-client example

---
+++ examples/iam-client/src/main.rs
@@ -0,0 +1,37 @@
+use gcloud_sdk::google::iam::credentials::v1::iam_credentials_client::IamCredentialsClient;
+use gcloud_sdk::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Debug logging
+    let subscriber = tracing_subscriber::fmt()
+        .with_env_filter("gcloud_sdk=debug")
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    let client: GoogleApi<IamCredentialsClient<GoogleAuthMiddleware>> = GoogleApi::from_function(
+        IamCredentialsClient::new,
+        "https://iamcredentials.googleapis.com",
+        // cloud resource prefix: used only for some of the APIs (such as Firestore)
+        None,
+    )
+    .await?;
+
+    // Option 1: using service account impersonation
+    let resp = client
+        .get()
+        .sign_blob(
+            tonic::Request::new(
+                gcloud_sdk::google::iam::credentials::v1::SignBlobRequest {
+                    name: "projects/-/serviceAccounts/latestbit@appspot.gserviceaccount.com".to_string(),
+                    delegates: vec![],
+                    payload: "https://storage.googleapis.com/example-bucket/cat.jpeg".as_bytes().to_vec(),
+                }
+            )
+        )
+        .await?;
+
+    println!("Response: {:x?}", resp.into_inner().signed_blob);
+
+    Ok(())
+}
