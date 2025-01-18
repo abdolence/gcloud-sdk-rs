39ec6665c832fa2c813bf2692288e8038a5b5e9c
Abdulla Abdurakhmanov <me@abdolence.dev>
2025-01-18 15:18:49 +0100

    fix: Patched GCE API spec for compute_instances_stop for empty-body issue

--- gcloud-sdk/src/rest_apis/google_rest_apis/compute_v1/apis/instances_api.rs
+++ gcloud-sdk/src/rest_apis/google_rest_apis/compute_v1/apis/instances_api.rs
@@ -1588,6 +1588,7 @@ pub struct ComputePeriodInstancesPeriodStopParams {
     pub discard_local_ssd: Option<bool>,
     /// An optional request ID to identify requests. Specify a unique request ID so that if you must retry your request, the server will know to ignore the request if it has already been completed. For example, consider a situation where you make an initial request and the request times out. If you make the request again with the same request ID, the server can check if original operation with the same request ID was received, and if so, will ignore the second request. This prevents clients from accidentally creating duplicate commitments. The request ID must be a valid UUID with the exception that zero UUID is not supported ( 00000000-0000-0000-0000-000000000000).
     pub request_id: Option<String>,
+    pub body: Option<serde_json::Value>,
 }
 
 /// struct for passing parameters to the method [`compute_instances_suspend`]
@@ -7078,6 +7079,7 @@ pub async fn compute_instances_stop(
     let user_ip = params.user_ip;
     let discard_local_ssd = params.discard_local_ssd;
     let request_id = params.request_id;
+    let body = params.body;
 
     let local_var_client = &local_var_configuration.client;
 
@@ -7155,6 +7157,7 @@ pub async fn compute_instances_stop(
     if let Some(ref local_var_token) = local_var_configuration.oauth_access_token {
         local_var_req_builder = local_var_req_builder.bearer_auth(local_var_token.to_owned());
     };
+    local_var_req_builder = local_var_req_builder.json(&body);
 
     let local_var_req = local_var_req_builder.build()?;
     let local_var_resp = local_var_client.execute(local_var_req).await?;
