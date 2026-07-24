822d9c4a2f47892e4291fdd33bfc676d30a9625f
Abdulla Abdurakhmanov <me@abdolence.dev>
2026-07-24 16:01:23 +0200

    chore: Remove unusued protos gen

--- gcloud-protos-generator/src/main.rs
+++ gcloud-protos-generator/src/main.rs
@@ -58,7 +58,7 @@ fn filter_unwanted_protos(protos: Vec<Proto>) -> Vec<Proto> {
             !path.contains("google/ads/")
                 && !path.contains("google/cloud/compute/v1beta/")
                 && !path.contains("google/cloud/gkehub/")
-                && !path.contains("google.cloud.retail/")
+                && !path.contains("google/cloud/retail/")
                 && !path.contains("google/shopping")
                 && !path.contains("preview/google/")
         })
